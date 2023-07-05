56da0d6aa3f275e0d45bf576e9774fdd6c1d3563
Tyrn <a@b.com>
2023-07-05 19:41:51 +0300

    Mappings to after/ftplugin/tex.lua, return{} to lua/user/plugins/vimtex.lua

--- dot_config/nvim/after/ftplugin/tex.lua
+++ dot_config/nvim/after/ftplugin/tex.lua
@@ -26,24 +26,3 @@ keymap(0, "i", "БЬ", "«", opts)
 keymap(0, "i", "ЮЬ", "»", opts)
 keymap(0, "i", "ññ", "«", opts)
 keymap(0, "i", "ÑÑ", "»", opts)
-
-return {
-  -- VImTex configuration
-  {
-    "lervag/vimtex",
-    init = function()
-      vim.g.tex_flavor = "latex"
-      vim.g.vimtex_view_method = "general"
-      vim.g.vimtex_view_general_viewer = "okular"
-      vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
-      -- vim.g.vimtex_view_general_options_latexmk = '--unique'
-      vim.g.vimtex_compiler_progname = "nvr"
-      vim.g.vimtex_compiler_method = "latexmk"
-      vim.g.vimtex_compiler_latexmk = {
-        background = 1,
-        continuous = 1,
-      }
-    end,
-    event = "BufRead",
-  },
-}
