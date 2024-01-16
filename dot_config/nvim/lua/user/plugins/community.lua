44441d95e2192012a3a0805ee74400d89692e835
Tyrn <a@b.com>
2024-01-16 08:26:18 +0300

    astrocommunity.pack.kotlin to user/plugins/community.lua

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -7,4 +7,5 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.kotlin" },
 }
