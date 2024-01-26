152b63d97cdfedfda051242d61c2acf0865c2c27
Tyrn <a@b.com>
2024-01-26 19:27:42 +0300

    VimTeX viewer: Okular to Zathura

--- dot_config/nvim/lua/user/plugins/vimtex.lua
+++ dot_config/nvim/lua/user/plugins/vimtex.lua
@@ -2,13 +2,10 @@ return {
 	-- VImTex configuration
 	{
 		"lervag/vimtex",
+		lazy = false,
 		init = function()
 			vim.g.tex_flavor = "latex"
-			vim.g.vimtex_view_method = "general"
-			vim.g.vimtex_view_general_viewer = "okular"
-			vim.g.vimtex_view_general_options = "--unique file:@pdf#src:@line@tex"
-			-- vim.g.vimtex_view_general_options_latexmk = '--unique'
-			vim.g.vimtex_compiler_progname = "nvr"
+			vim.g.vimtex_view_method = "zathura"
 			vim.g.vimtex_compiler_method = "latexmk"
 			vim.g.vimtex_compiler_latexmk = {
 				background = 1,
