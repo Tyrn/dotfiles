a83eb95f82ddd773b1048295cb34d08bed33c9ba
Tyrn <a@b.com>
2024-02-09 22:05:41 +0300

    elm to community.lua

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -1,11 +1,12 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.kotlin" },
+	-- Add the community repository of plugin specifications
+	"AstroNvim/astrocommunity",
+	-- example of imporing a plugin, comment out to use it or add your own
+	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	-- { import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.kotlin" },
+	{ import = "astrocommunity.pack.elm" },
 }
