083258ad6b1645929f8e993fc5c18f64b65ecd8b
Tyrn <a@b.com>
2024-03-22 16:40:22 +0300

    racket to dot_doom.d/init.el

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -172,7 +172,7 @@
          +lsp
          +pyright)           ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
+       (racket +lsp)           ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
