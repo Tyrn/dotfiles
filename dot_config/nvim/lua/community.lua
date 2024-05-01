800c62f25e1f1d74fe66e8dae51d7d3c4e3720fb
Tyrn <a@b.com>
2024-05-01 19:29:10 +0300

    First attempt at AstroNvim 4.0

---
+++ dot_config/nvim/lua/community.lua
@@ -0,0 +1,20 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.kotlin" },
+	{ import = "astrocommunity.pack.json" },
+	{ import = "astrocommunity.pack.elm" },
+	{ import = "astrocommunity.pack.nix" },
+	{ import = "astrocommunity.pack.haskell" },
+	{ import = "astrocommunity.pack.typst" },
+  -- import/override with your plugins folder
+}
