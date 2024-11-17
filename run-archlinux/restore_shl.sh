921386fdfa53de1587d489ef613bf4497abac085
Tyrn <alexey1911@gmail.com>
2024-11-17 11:29:12 +0300

    zsh plugin patches adapted

--- run-archlinux/restore_shl.sh
+++ run-archlinux/restore_shl.sh
@@ -4,19 +4,19 @@
 #|-/ /--| Prasanth Rangan           |-/ /--|#
 #|/ /---+---------------------------+/ /---|#
 
-scrDir=$(dirname "$(realpath "$0")")
-source "${scrDir}/global_fn.sh"
-if [ $? -ne 0 ]; then
-    echo "Error: unable to source global_fn.sh..."
-    exit 1
-fi
-
-if chk_list "myShell" "${shlList[@]}"; then
-    echo -e "\033[0;32m[SHELL]\033[0m detected // ${myShell}"
-else
-    echo "Error: user shell not found"
-    exit 1
-fi
+#scrDir=$(dirname "$(realpath "$0")")
+#source "${scrDir}/global_fn.sh"
+#if [ $? -ne 0 ]; then
+#    echo "Error: unable to source global_fn.sh..."
+#    exit 1
+#fi
+#
+#if chk_list "myShell" "${shlList[@]}"; then
+#    echo -e "\033[0;32m[SHELL]\033[0m detected // ${myShell}"
+#else
+#    echo "Error: user shell not found"
+#    exit 1
+#fi
 
 # add zsh plugins
 if pkg_installed zsh && pkg_installed oh-my-zsh-git; then
@@ -40,15 +40,15 @@ if pkg_installed zsh && pkg_installed oh-my-zsh-git; then
         fi
     done < <(cut -d '#' -f 1 "${scrDir}/restore_zsh.lst" | sed 's/ //g')
 
-    # update plugin array in zshrc
-    echo -e "\033[0;32m[SHELL]\033[0m installing plugins (${w_plugin} )"
-    sed -i "/^plugins=/c\plugins=(${w_plugin} )${Fix_Completion}" "${Zsh_rc}"
+#    # update plugin array in zshrc
+#    echo -e "\033[0;32m[SHELL]\033[0m installing plugins (${w_plugin} )"
+#    sed -i "/^plugins=/c\plugins=(${w_plugin} )${Fix_Completion}" "${Zsh_rc}"
 fi
 
-# set shell
-if [[ "$(grep "/${USER}:" /etc/passwd | awk -F '/' '{print $NF}')" != "${myShell}" ]]; then
-    echo -e "\033[0;32m[SHELL]\033[0m changing shell to ${myShell}..."
-    chsh -s "$(which "${myShell}")"
-else
-    echo -e "\033[0;33m[SKIP]\033[0m ${myShell} is already set as shell..."
-fi
+## set shell
+#if [[ "$(grep "/${USER}:" /etc/passwd | awk -F '/' '{print $NF}')" != "${myShell}" ]]; then
+#    echo -e "\033[0;32m[SHELL]\033[0m changing shell to ${myShell}..."
+#    chsh -s "$(which "${myShell}")"
+#else
+#    echo -e "\033[0;33m[SKIP]\033[0m ${myShell} is already set as shell..."
+#fi
