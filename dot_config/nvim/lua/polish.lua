665659d1d165755953be066a568ea56838c29fa4
Tyrn <alexey1911@gmail.com>
2025-03-27 19:33:20 +0300

    polish.lua adopted (heading) from AstroNvim v5

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -4,6 +4,19 @@
 -- things like custom filetypes. This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+-- Set up custom filetypes
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
 -- Spell settings
 vim.opt.spelllang = { "ru_ru", "en_us", "es_es" }
 
