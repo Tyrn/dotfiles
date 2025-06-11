68fabc5e2a71c11d1aa4ad94a28375aed19b4d43
Tyrn <alexey1911@gmail.com>
2025-06-11 15:26:19 +0300

    plugins/neorg.lua: core.concealer added

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -5,6 +5,15 @@ return {
 		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 		version = "*", -- Pin Neorg to the latest stable release
 		config = true,
-		init = function() end,
+		init = function()
+			require("neorg").setup({
+				load = {
+					["core.concealer"] = {
+						-- Configuration options go here
+					},
+					-- Your other modules...
+				},
+			})
+		end,
 	},
 }
