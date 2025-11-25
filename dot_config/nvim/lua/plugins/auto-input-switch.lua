ab3c60226f41f7552bd2adaf140ec19ffe62d70e
Tyrn <alexey1911@gmail.com>
2025-11-25 15:08:14 +0300

    auto-input-switch.lua to plugins

---
+++ dot_config/nvim/lua/plugins/auto-input-switch.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "amekusa/auto-input-switch.nvim",
+    lazy = false,
+    opts = {
+      os_settings = {
+        linux = {
+          cmd_get = "get_keyboard_layout_gnome", -- set the command that outputs the keyboard state
+          cmd_set = "set_keyboard_layout_gnome %s", -- set the command that changes the keyboard state
+        },
+      },
+    },
+  },
+}
