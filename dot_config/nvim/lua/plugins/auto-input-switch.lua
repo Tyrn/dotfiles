a9d00cfaa4bf09f6393f71d88564eb6a2d25d5ab
Tyrn <alexey1911@gmail.com>
2025-11-25 15:53:25 +0300

    auto-input-switch.lua disabled for the time being

--- dot_config/nvim/lua/plugins/auto-input-switch.lua
+++ dot_config/nvim/lua/plugins/auto-input-switch.lua
@@ -5,8 +5,8 @@ return {
     opts = {
       os_settings = {
         linux = {
-          cmd_get = "get_keyboard_layout_gnome", -- set the command that outputs the keyboard state
-          cmd_set = "set_keyboard_layout_gnome %s", -- set the command that changes the keyboard state
+          -- cmd_get = "get_keyboard_layout_gnome", -- set the command that outputs the keyboard state
+          -- cmd_set = "set_keyboard_layout_gnome %s", -- set the command that changes the keyboard state
         },
       },
     },
