d9f3908edba933ad9ef1a6041f4c693397db2307
Tyrn <alexey1911@gmail.com>
2025-11-26 23:51:15 +0300

    auto-input-switch.lua: check for GNOME

--- dot_config/nvim/lua/plugins/auto-input-switch.lua
+++ dot_config/nvim/lua/plugins/auto-input-switch.lua
@@ -1,21 +1,33 @@
-return {
-  {
-    "amekusa/auto-input-switch.nvim",
-    lazy = false,
-    opts = {
-      os_settings = {
-        linux = {
-          cmd_get = vim.fn.stdpath "config" .. "/gnome-kbd.sh", -- set the command that outputs the keyboard state
-          cmd_set = vim.fn.stdpath "config" .. "/gnome-kbd.sh %s", -- set the command that changes the keyboard state
-          -- normal_input = "us", -- Optional.
-          lang_inputs = {
-            Ru = "ru",
-            Es = "es",
-            Slavonic = "m17n:cu:kbd",
-            Hellenic = "m17n:grc:mizuochi",
+if os.getenv "XDG_CURRENT_DESKTOP" == "GNOME" then
+  return {
+    {
+      "amekusa/auto-input-switch.nvim",
+      lazy = false,
+      opts = {
+        os_settings = {
+          linux = {
+            cmd_get = vim.fn.stdpath "config" .. "/gnome-kbd.sh", -- set the command that outputs the keyboard state
+            cmd_set = vim.fn.stdpath "config" .. "/gnome-kbd.sh %s", -- set the command that changes the keyboard state
+            -- normal_input = "us", -- Optional.
+            lang_inputs = {
+              Ru = "ru",
+              Es = "es",
+              Slavonic = "m17n:cu:kbd",
+              Hellenic = "m17n:grc:mizuochi",
+            },
           },
         },
       },
     },
-  },
-}
+  }
+else
+  return {
+    {
+      "amekusa/auto-input-switch.nvim",
+      lazy = false,
+      opts = {
+        -- log = true,
+      },
+    },
+  }
+end
