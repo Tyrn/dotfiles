66e980dccd469e7100673c89a8ebbf4622f6b47e
Tyrn <alexey1911@gmail.com>
2025-11-26 22:27:59 +0300

    First experience with auto-input-switch.lua

--- dot_config/nvim/lua/plugins/auto-input-switch.lua
+++ dot_config/nvim/lua/plugins/auto-input-switch.lua
@@ -5,8 +5,15 @@ return {
     opts = {
       os_settings = {
         linux = {
-          -- cmd_get = "get_keyboard_layout_gnome", -- set the command that outputs the keyboard state
-          -- cmd_set = "set_keyboard_layout_gnome %s", -- set the command that changes the keyboard state
+          cmd_get = vim.fn.stdpath "config" .. "/gnome-kbd.sh", -- set the command that outputs the keyboard state
+          cmd_set = vim.fn.stdpath "config" .. "/gnome-kbd.sh %s", -- set the command that changes the keyboard state
+          -- normal_input = "us", -- Optional.
+          lang_inputs = {
+            Ru = "ru",
+            Es = "es",
+            Slavonic = "m17n:cu:kbd",
+            Hellenic = "m17n:grc:mizuochi",
+          },
         },
       },
     },
