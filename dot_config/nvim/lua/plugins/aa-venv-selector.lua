f6ec9ca579af5d9e143c05eb3d02f1f13101de8d
Tyrn <alexey1911@gmail.com>
2026-03-15 17:59:45 +0300

    lua/plugins/aa-venv-selector.lua added

---
+++ dot_config/nvim/lua/plugins/aa-venv-selector.lua
@@ -0,0 +1,31 @@
+if false then
+  return {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope.nvim", version = "*", dependencies = { "nvim-lua/plenary.nvim" } }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
+    },
+    ft = "python", -- Load when opening Python files
+    -- keys = { { ",v", "<cmd>VenvSelect<cr>" } }, -- Open picker on keymap
+    keys = {
+      -- Keymap to open the selector
+      { "<leader>Vv", "<cmd>VenvSelect<cr>" },
+    },
+    opts = {
+      options = {}, -- plugin-wide options
+      search = {}, -- custom search definitions
+    },
+  }
+else
+  return {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+      name = ".venv", -- Tell it to look for your uv environment
+      auto_refresh = true,
+    },
+    keys = {
+      -- Keymap to open the selector
+      { "<leader>Vs", "<cmd>VenvSelect<cr>" },
+    },
+  }
+end
