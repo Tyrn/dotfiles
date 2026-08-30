5765c6ec8999b3ed50e1cf8bad2b358e058fcf23
Tyrn <alexey1911@gmail.com>
2026-08-30 23:04:07 +0300

    moji(): leading spaces refactored

--- dot_assorted-helpers.sh
+++ dot_assorted-helpers.sh
@@ -87,12 +87,15 @@ function scc() {
 function moji() {
   local n=4
   local w=$((n * 3))
-  printf "   %.0s" $(seq 1 $n)
+  local spaces
+  printf -v spaces "   %.0s" $(seq 1 $n)
+
+  printf "%s" "$spaces"
   printf " <>%.0s" $(seq 1 $w)
   echo ""
-  printf "   %.0s" $(seq 1 $n)
+  printf "%s" "$spaces"
   echo "|\u2764 \u26ac\u2b50 \U1f344 \U1f600 \U1f34a \u2728 \U1f340 \U0001f337 \U1fabb \U1f981 \U1f4A1 \U1f341|"
-  printf "   %.0s" $(seq 1 $n)
+  printf "%s" "$spaces"
   echo "|\U1f33d \U1f33e \U1f490 \U1f940 \U1f342 \U1f982 \U1f98b \U1f41e \U1f3f0 \U1f49b \U1f3b2 \U1f352|"
 }
 
