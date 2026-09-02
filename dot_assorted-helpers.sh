6d0721f2af2290f6abcef227cff996019a29f312
Tyrn <alexey1911@gmail.com>
2026-09-02 22:09:34 +0300

    moji(): another ugly icon to show

--- dot_assorted-helpers.sh
+++ dot_assorted-helpers.sh
@@ -92,7 +92,7 @@ function moji() {
   printf " <>%.0s" {1..12}
   echo ""
   printf "%s" "$spaces"
-  echo "|\u2764 \u26ac\u2b50 \U1f344 \U1f600 \U1f34a \u2728 \U1f340 \U0001f337 \U1fabb \U1f981 \U1f4A1 \U1f341|"
+  echo "|\u2764 \u26ac\u2b50 \U1f344 \U1f600 \U1f34a \u2728 \U1f340 \U0001f337 \U1fabb \U1f981 \u26a0 \u26ac\U1f341|"
   printf "%s" "$spaces"
   echo "|\U1f33d \U1f33e \U1f490 \U1f940 \U1f342 \U1f982 \U1f98b \U1f41e \U1f3f0 \U1f49b \U1f3b2 \U1f352|"
   printf "%s" "$spaces"
